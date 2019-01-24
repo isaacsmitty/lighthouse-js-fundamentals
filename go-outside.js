41a57fcf06c733af80f03464eca61bf02165dcdc
Isaac Smith <isaac.smitty@gmail.com>
2019-01-24 17:37:33 +0000

    function go-outside.js with if else statement

---
+++ go-outside.js
@@ -0,0 +1,12 @@
+//var raining = true;
+var temp = 0;
+
+if (temp <= 0) {
+  console.log('Make sure you pick out a scarf!');
+} else if (temp < 15){
+  console.log('Short sleeves won\'t cut it.')
+} else {
+  console.log('Short sleeves are fine.')
+}
+
+console.log('Now you\'re ready to go outside!');
