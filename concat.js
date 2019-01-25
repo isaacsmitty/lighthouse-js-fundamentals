0ef0d9f3addbcfda252acb88cb77faac6e2a8b98
Isaac Smith <isaac.smitty@gmail.com>
2019-01-25 06:15:43 +0000

    array assignments, concat & merge

---
+++ concat.js
@@ -0,0 +1,9 @@
+function concat (first, second) {
+  var combined = first.concat(second);
+  return combined
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ])
---
+++ merge.js
@@ -0,0 +1,9 @@
+function merge (first, second) {
+  var combined = first.concat(second)
+  combined.sort();
+  return combined
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
